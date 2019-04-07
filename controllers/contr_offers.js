603555668b3e3fe62823a2ed5badd1c2eae1994d
Yannik Goldgräbe <ykg@live.de>
2019-04-07 15:09:14 +0200

    Fine tuning offers

--- controllers/contr_offers.js
+++ controllers/contr_offers.js
@@ -2,9 +2,23 @@ var models = require('../models/mdl_generics');
 
 var controller = {
     getOffers: function(req, callback){
-        models.findAll('locations', {}, {}, function(err, result){
+
+        var period = new Date();
+        period.setHours(period.getHours() - 6);
+
+        models.findAll('locations', {'time' : {$gte: period}}, {}, function(err, result){
             if(!err){
+                //Let the smaller radius offers be above the bigger ones -> they can be clicked in frontend
                 result.sort(function(a, b){return b.radius-a.radius});
+
+                //Remove all the expired offers
+                result.forEach(function(item, index, object) {
+                    var selectedDuration = item.duration * 60 * 60 * 1000; /* ms */
+                    if((new Date() - new Date(item.time)) > selectedDuration){
+                        object.splice(index, 1);
+                    }
+                });
+                
                 callback(result);
             } else {
                 console.error(err);
